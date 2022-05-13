d5a5da4ae27ffa31737d7d62411e0bf2c5285f5c
Rajdeep Kundu <rajdeepkundu2983@gmail.com>
2022-05-13 21:47:35 +0530

    Some mild changes made

--- Hashing-4.c
+++ Hashing-4.c
@@ -5,11 +5,17 @@
 #include<math.h>
 int n;
 int insert;
+int counter;
 struct node 
 {
     int info;
     struct node *next;
 };typedef struct node node;
+node* insert_node_at_last(node*start,int k);
+node* Open_Hashing(node *h[],int k);
+node *create_node(int k);
+int H(int k);
+void display(node *start);
 node *create_node(int k)
 {
     node *p;
@@ -24,7 +30,7 @@ int H(int k)
     val=k%n;
     return val;
 }
-void insert_node_at_last(node*start,int k)//inserting node at the end of the list
+node* insert_node_at_last(node*start,int k)//inserting node at the end of the list
 {
     node*p,*q;
     p=create_node(k);// new node created
@@ -41,14 +47,36 @@ void insert_node_at_last(node*start,int k)//inserting node at the end of the lis
         }
         q->next=p;
     }
+    return start;
 }
-void Open_Hashing(node *h[],int k)
+void display_counter(node *h[0])
+{
+    int i=0;
+    node*d;
+    for(i=0;i<n;i++)
+    {
+        d=h[i];
+        printf("\nH[%d]:",i);
+        display(d);
+    }
+}
+void display(node *d)
+{
+    node*r=NULL;
+    r=d;
+    while(r!=NULL)
+    {
+        printf("[Info->%d|Address_of_next_node->(%x)]-->>](address of this node->(%x))---->>>>\n",r->info,r->next,r);
+        r=r->next;
+    }
+}
+node* Open_Hashing(node *h[],int k)
 {
     int index;
     index=H(k);
     node*ptr;
     ptr=h[index];
-    int flag=0,counter=0;
+    int flag=0;
     while(ptr!=NULL && !flag) 
     {
         if(ptr->info==k)
@@ -56,32 +84,220 @@ void Open_Hashing(node *h[],int k)
             flag=1;
             counter++;
             printf("\nValue is present at:");
-            printf("\nNode.No:%d",counter);
-            printf("[Info->%d|Address_of_next_node->(%x)]-->>](address of this node->(%x))---->>>>\n",ptr->info,ptr->next,ptr);
-            exit(0);
+            printf("\nH[%d]:",index);
+            return ptr;
         }
         else
         {
             ptr=ptr->next;
+            if(insert==0)
+            counter++;
         }
     }
     if(flag==0)
     {
         if(insert==1)
         {
-            insert_node_at_last(h[index],k);
+            h[index]=insert_node_at_last(h[index],k);
         }
     }
 }
 void main()
 {
-    int n,key,insert;
+    int key;
+    char ch;
+    node*ptr;
     node *h[1000]={NULL};
-    printf("\n Enter the length of Hash Table:\n");
+    printf("\nEnter the length of Hash Table:");
     scanf("%d",&n);
-    printf("\nEnter the element you want to insert:\n");
+    start:
+    printf("\nEnter the element you want to insert/search:");
     scanf("%d",&key);
-    printf("\nIf you want to insert press 1");
+    printf("\nIf you want to insert press 1:");
     scanf("%d",&insert);
-    Open_Hashing(h,key);
+    ptr=Open_Hashing(h,key);
+    if(insert==0)
+    {
+        printf("Node.No:%d->",counter);
+        counter=0;
+        printf("[Info->%d|Address_of_next_node->(%x)]-->>](address of this node->(%x))---->>>>\n",ptr->info,ptr->next,ptr);
+    }
+    display_counter(h);
+    printf("\n If you want to continue the process the press Y or y");
+    printf("\n-->>");
+    ch=getch();
+    if (ch=='Y' || ch=='y')
+    {
+      goto start;
+    }
 }
+/* ********************************OUTPUT********************************
+Enter the length of Hash Table:10
+
+Enter the element you want to insert/search:1
+
+If you want to insert press 1:1
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:
+H[3]:
+H[4]:
+H[5]:
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:12
+
+If you want to insert press 1:1
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(0)]-->>](address of this node->(54711410))---->>>>
+
+H[3]:
+H[4]:
+H[5]:
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:45
+
+If you want to insert press 1:1
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(0)]-->>](address of this node->(54711410))---->>>>
+
+H[3]:
+H[4]:
+H[5]:[Info->45|Address_of_next_node->(0)]-->>](address of this node->(54711430))---->>>>
+
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:55
+
+If you want to insert press 1:1
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(0)]-->>](address of this node->(54711410))---->>>>
+
+H[3]:
+H[4]:
+H[5]:[Info->45|Address_of_next_node->(54711450)]-->>](address of this node->(54711430))---->>>>
+[Info->55|Address_of_next_node->(0)]-->>](address of this node->(54711450))---->>>>
+
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:22
+
+If you want to insert press 1:1
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(54711470)]-->>](address of this node->(54711410))---->>>>
+[Info->22|Address_of_next_node->(0)]-->>](address of this node->(54711470))---->>>>
+
+H[3]:
+H[4]:
+H[5]:[Info->45|Address_of_next_node->(54711450)]-->>](address of this node->(54711430))---->>>>
+[Info->55|Address_of_next_node->(0)]-->>](address of this node->(54711450))---->>>>
+
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:35
+
+If you want to insert press 1:1
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(54711470)]-->>](address of this node->(54711410))---->>>>
+[Info->22|Address_of_next_node->(0)]-->>](address of this node->(54711470))---->>>>
+
+H[3]:
+H[4]:
+H[5]:[Info->45|Address_of_next_node->(54711450)]-->>](address of this node->(54711430))---->>>>
+[Info->55|Address_of_next_node->(54711490)]-->>](address of this node->(54711450))---->>>>
+[Info->35|Address_of_next_node->(0)]-->>](address of this node->(54711490))---->>>>
+
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:12
+
+If you want to insert press 1:0
+
+Value is present at:
+H[2]:Node.No:1->[Info->12|Address_of_next_node->(54711470)]-->>](address of this node->(54711410))---->>>>
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(54711470)]-->>](address of this node->(54711410))---->>>>
+[Info->22|Address_of_next_node->(0)]-->>](address of this node->(54711470))---->>>>
+
+H[3]:
+H[4]:
+H[5]:[Info->45|Address_of_next_node->(54711450)]-->>](address of this node->(54711430))---->>>>
+[Info->55|Address_of_next_node->(54711490)]-->>](address of this node->(54711450))---->>>>
+[Info->35|Address_of_next_node->(0)]-->>](address of this node->(54711490))---->>>>
+
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>
+Enter the element you want to insert/search:35
+
+If you want to insert press 1:0
+
+Value is present at:
+H[5]:Node.No:3->[Info->35|Address_of_next_node->(0)]-->>](address of this node->(54711490))---->>>>
+
+H[0]:
+H[1]:[Info->1|Address_of_next_node->(0)]-->>](address of this node->(547113f0))---->>>>
+
+H[2]:[Info->12|Address_of_next_node->(54711470)]-->>](address of this node->(54711410))---->>>>
+[Info->22|Address_of_next_node->(0)]-->>](address of this node->(54711470))---->>>>
+
+H[3]:
+H[4]:
+H[5]:[Info->45|Address_of_next_node->(54711450)]-->>](address of this node->(54711430))---->>>>
+[Info->55|Address_of_next_node->(54711490)]-->>](address of this node->(54711450))---->>>>
+[Info->35|Address_of_next_node->(0)]-->>](address of this node->(54711490))---->>>>
+
+H[6]:
+H[7]:
+H[8]:
+H[9]:
+ If you want to continue the process the press Y or y
+-->>*/
