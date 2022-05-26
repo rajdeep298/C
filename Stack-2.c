149347744e6a2bbffd91799bfb73724bb9c6a675
Rajdeep Kundu <rajdeepkundu2983@gmail.com>
2022-05-26 21:44:12 +0530

    Stack-2.c Created by Rajdeep

---
+++ Stack-2.c
@@ -0,0 +1,76 @@
+/*Unlimited Stack*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void display_stack();
+void push_stack(int n);
+void pop_stack();
+int top=-1;
+int a[]={0};
+void display_stack()
+{
+    int i;
+    for(i=top;i>=0;i--)
+    {
+        printf("\n Element-%d:%d",i,a[i]);
+    }
+    printf("\n This was output of your chosen opeartion :) .");
+}
+void push_stack(int n)
+{
+   int value;
+    top=top+1;
+    printf("\n Enter the value to push: ");
+    scanf("%d",&value);
+    a[top]=value;
+   display_stack();
+}
+void pop_stack()
+{
+    int value;
+    if(top<0)
+    {
+        printf("\n Stack underflow");
+    }
+    else
+    {
+      value=a[top];
+      top=top-1;
+      printf("\nValue extracted from stack: %d",value);
+    }
+    display_stack();
+}
+void main()
+{
+    int choice;
+    int n,a[100];
+    char ch;
+    system("cls");
+    printf("\n Enter the limit of the stack:");
+    scanf("%d",&n);
+    start:
+    printf("\nPress 1 to PUSH\nPress 2 for POP\nPress 3 for Displaying the Stack\nPress 4 for exit the process.");
+    printf("\n-->>");
+    scanf("%d",&choice);
+switch(choice)
+{
+    case 1: push_stack(n);
+            break;
+    case 2: pop_stack();
+            break;
+    case 3:display_stack();
+            break;
+    case 4:exit(0);
+	       break;
+    default: printf("\n Enter the correct choice");                             
+}
+    printf("\n If you want to continue, press Y or y ");
+    printf("\n-->>");
+    ch=getch();
+    if(ch=='y'|| ch=='Y')
+    {
+        goto start;
+    }
+    printf("\n ********************End of the process********************");
+    printf("\n:)");
+}
--- x.c
+++ x.c
@@ -1,42 +1,25 @@
+/* Dyanmic Array*/
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-//Complete the following function.
-
-
-void calculate_the_maximum(int n, int k) {
-int i,j,r1,r2,r3,max1=0,max2=0,max3=0,comp;
-comp=(n<k)?n:k;
-for(i=1;i<=n;i++)
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+int *base=NULL;
+void main() 
 {
-    for(j=1;j<=k && i!=j ;j++)
-    {
-    r1=i&j;
-    r2=i|j;
-    r3=i^j;
-    if(max1<=r1 &&r1<comp)
-    {
-        max1=r1;
-    }
-    if(max2<=r2&& r2<comp )
-    {
-      max2=r2;
-    }
-    if(max3<=r3&& r3<comp)
-    {
-      max3=r3;
-    }
-    }
-}
-printf("%d\n%d\n%d",max1,max2,max3);
-}
-
-int main() {
-    int n, k;
   
-    scanf("%d %d", &n, &k);
-    calculate_the_maximum(n, k);
- 
-    return 0;
+  int *ptr=NULL,n,i;
+  printf("\n Enter the number of elements in the array: ");
+  scanf("%d",&n);
+  base = (int*)malloc(n*sizeof(int));
+  for(i=0;i<n;i++)
+  {
+    printf("\nEnter the element: ");
+    scanf("%d",*(ptr+i));
+  }
+  printf("\nElements of the array: ");
+  for(i=0;i<n;i++)
+  {
+    printf("\nElements of the array-%d::%d",i,*(ptr+i));
+  }
+  
 }
