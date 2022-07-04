526a63e07c08f9b5e5e45ecaa8e47714917bca01
Rajdeep Kundu <rajdeepkundu2983@gmail.com>
2022-07-04 22:52:27 +0530

    some mild changes

--- Queue-2.c
+++ Queue-2.c
@@ -95,6 +95,7 @@ void main()
                 {
                     circular_queue_display_deletion(a,max);
                 }
+                break;
         case 4:printf("\nExiting the Programme .......");
                exit(0);
                break;
@@ -110,3 +111,86 @@ void main()
     printf("\n********************End of the process********************");
     printf("\n:)");
 }
+/*
+Enter the limit of the queue: 5
+
+Press
+1.For Insertion,
+2 for Deletion,
+3 for Display,
+4 for Exiting the Programme
+Enter the choice:1
+
+Insertion-->>
+Enter the Number to insert in the Queue:1
+
+Element-0:1
+This was output of your chosen opeartion :) .
+If you want to continue, press Y or y.
+-->>
+Press
+1.For Insertion,
+2 for Deletion,
+3 for Display,
+4 for Exiting the Programme
+Enter the choice:1
+
+Insertion-->>
+Enter the Number to insert in the Queue:2
+
+Element-0:1
+Element-1:2
+This was output of your chosen opeartion :) .
+If you want to continue, press Y or y.
+-->>
+Press
+1.For Insertion,
+2 for Deletion,
+3 for Display,
+4 for Exiting the Programme
+Enter the choice:1
+
+Insertion-->>
+Enter the Number to insert in the Queue:3
+
+Element-0:1
+Element-1:2
+Element-2:3
+This was output of your chosen opeartion :) .
+If you want to continue, press Y or y.
+-->>
+Press
+1.For Insertion,
+2 for Deletion,
+3 for Display,
+4 for Exiting the Programme
+Enter the choice:2
+
+Deletion-->>
+The value extracted from the Queue:1
+Element-1:2
+Element-2:3
+This was output of your chosen opeartion :) .
+If you want to continue, press Y or y.
+-->>
+Press
+1.For Insertion,
+2 for Deletion,
+3 for Display,
+4 for Exiting the Programme
+Enter the choice:3
+
+Display-->>
+Element-1:2
+Element-2:3
+This was output of your chosen opeartion :) .
+If you want to continue, press Y or y.
+-->>
+Press
+1.For Insertion,
+2 for Deletion,
+3 for Display,
+4 for Exiting the Programme
+Enter the choice:4
+
+Exiting the Programme .......*/
