b649d2fc159cfba4c9308dff99dd25a393929065
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-04-27 23:08:13 +0530

    some mild changes

---
+++ Depth_First_Search.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+struct graph_node{
+    int index_number;
+    bool visited;
+};
+typedef struct graph_node node;
+void main() {
+    int n,i,j,top=-1,data,index=0;
+    printf("\nEnter the number of nodes:");
+    scanf("%d", &n);
+    int a[n][n];
+    node stack[n];
+    for ( i = 0;i<n;i++){
+        printf("\nIf %d is connected to:",i+1);
+        for( j = 0;j<n;j++){
+            if(i==j){
+                a[i][j] =0;
+                continue;
+            }
+            else
+            {
+                printf("\n%d press 1:",j+1);
+                scanf("%d",&a[i][j]);
+
+            }
+        }
+    }
+    printf("\nThe adjacency matrix is:\n");
+    for(i=0;i<n;i++){
+        stack[i].visited=false;
+        for(j=0;j<n;j++){
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    top++;
+    
+    printf("\nConnected Vertices:");
+}
