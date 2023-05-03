63737a66fa13db645214185f3d92aa3cff300f04
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-05-03 23:02:12 +0530

    some mild changes

--- Graph_Colouring_Problem.c
+++ Graph_Colouring_Problem.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-struct colours {
-    char str[100];
-    char status;
-}c[100];
-struct graph {
+struct graph{
     int a[100][100];
     int n;
 }g;
-void Graph_Colouring_Problem();
-void Graph_Colouring_Problem(){
+struct color{
+    char color_name[100];
+    int vertex;
+    char status;
+}c[100];
+void graph_colouring_method();
+void graph_colouring_method(){
+    int i, j,k;
     printf("\nEnter the number of vertices:");
-    scanf("%d", &g.n);
+    scanf("%d",&g.n);
+    for ( i = 0;i<g.n;i++){
+        printf("\nIf %d is connected to:",i+1);
+        for( j = 0;j<g.n;j++){
+            if(i==j){
+                g.a[i][j] =0;
+                continue;
+            }
+            else
+            {
+                printf("\n%d press 1:",j+1);
+                scanf("%d",&g.a[i][j]);
+            }
+        }
+    }
+    c[0].vertex=0;
+    c[0].status='!';
+    
 }
